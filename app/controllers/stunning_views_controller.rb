00a7a9651b31be5305b6d5ed906a88762380fcb2
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-08-24 17:06:02 +0300

    Remove table name from SQL query.

--- app/controllers/stunning_views_controller.rb
+++ app/controllers/stunning_views_controller.rb
@@ -4,14 +4,14 @@ class StunningViewsController < ApplicationController
 
   def index
     params[:sort] ||= 'created_at'
-    order_by = "stunning_views.#{params[:sort]} DESC"
+    order_by = "#{params[:sort]} DESC"
     if params[:city_id].present?
       @stunning_views = City.find_by(id: params[:city_id]).stunning_views.order created_at: :desc
     elsif params[:longitude].present? && params[:latitude].present?
       flash.now[:info] = 'Showing views near you.'
       @stunning_views = StunningView.near([params[:latitude], params[:longitude]], 50).order created_at: :desc
     else
-      @stunning_views = StunningView.all.order order_by
+      @stunning_views = StunningView.order order_by
     end
   end
 
