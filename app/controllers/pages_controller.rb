ad0e66858169963f2b605849e06ee6ec4920e552
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2020-08-27 17:57:12 +0300

    Fix UI bugs and hacky code.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
   def home
-    #location = JSON.parse(RestClient.get("https://reallyfreegeoip.org/json/90.84.73.13"))
-    session[:current_latitude] = 31
-    session[:current_longitude] = 36
   end
 end
